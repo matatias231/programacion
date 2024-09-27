8510a2282fe11542face2d374a63846d42e92356
arturo <202313280j@utea.edu.pe>
2024-09-27 17:39:17 +0200

    hola

---
+++ OperacionesBasicas.java
@@ -0,0 +1,28 @@
+/**
+ * OperacionesBasicas
+ */
+public class OperacionesBasicas {
+
+    public static void main(String[] args) {
+        int numero1, numero2, suma, resta, multiplicacion;
+        double division;
+
+        System.out.println("Ingrese el primer numero:");
+        numero1 = Integer.parseInt(System.console().readLine());
+
+        System.out.println("Ingrese el sengundo numero:");
+        numero2 = Integer.parseInt(System.console().readLine());
+        
+        suma = numero1 + numero2;
+        resta =numero1 - numero2;
+        multiplicacion = numero1*numero2;
+        division = numero1/numero2;
+
+
+        System.out.println(suma);
+        System.out.println(resta);
+        System.out.println(multiplicacion);
+        System.out.println(division);
+
+    }
+}
